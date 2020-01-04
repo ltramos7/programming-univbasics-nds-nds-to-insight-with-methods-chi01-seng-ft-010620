5ec33688aaacfcac286196cdb4772cc3d98702c1
Linda Ramos <ltramos7@gmail.com>
2020-01-04 02:43:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -17,34 +17,6 @@ def gross_for_director(director_data)
 end
 
 
-
-
-# def directors_totals(nds)
-#   director_index = 0
-#   hash = {}
-#   director_name = 0 
-#   while director_index < directors_database.length do
-  
-#     movie_index = 0
-#     puts directors_database[director_index][:name]
-  
-#     total = 0
-#     while movie_index < directors_database[director_index][:movies].length do 
-#       total += directors_database[director_index][:movies][movie_index][:worldwide_gross]
-#       movie_index += 1
-#     end
-#     puts total
-#     director_name = directors_database[director_index][:name]
-#     hash[director_name] = total
-#     director_index += 1
-#   end
-#   hash
-# end
-
-# directors_totals (directors_database)
-
-#-----------------------------------------------------------------------------------
-
 #Write a method that, given an NDS creates a new Hash
 #The return value should be like:
 
